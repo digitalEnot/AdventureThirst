89a781c206960cac45b3589a3aa1ebab78881fd9
digitalEnot <evgeni.novikk@icloud.com>
2024-11-08 17:47:11 +0300

    Initial Commit

---
+++ AdventureThirst/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AdventureThirst
+//
+//  Created by Evgeni Novik on 08.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
